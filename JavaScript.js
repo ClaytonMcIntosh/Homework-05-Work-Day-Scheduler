7172e3266ab083abf8bbcf2ed4ad04bd2f4135b6
Clay <claymcintosh@gmail.com>
2022-01-12 22:45:44 +1000

    Built the row elements with bootstrap. Linked the classes to the time by getting time from moment, checking if it's past, present or future, and adding a class (already existing in the CSS).

--- JavaScript.js
+++ JavaScript.js
@@ -1,3 +1,34 @@
 //Set current day with moment
 let today = moment().format("dddd - MMM Do YYYY");
 $("#currentDay").html(today);
+
+//Color timeblock elements past present and future.
+
+timeColours();
+
+function timeColours() {
+  let hourTime = moment().hours();
+
+  //   console.log("hourTime = " + hourTime);
+
+  $(".time-block").each(function () {
+    let pastPresentFuture = parseInt($(".time-block").attr("id"));
+
+  console.log("pastPresentFuture1 = " + pastPresentFuture);
+    // console.log("hourTime = " + hourTime);
+
+    if (pastPresentFuture < hourTime) {
+      $(".time-block").removeClass("future");
+      $(".time-block").removeClass("present");
+      $(".time-block").addClass("past");
+    } else if (pastPresentFuture === hourTime) {
+      $(".time-block").removeClass("future");
+      $(".time-block").addClass("present");
+      $(".time-block").removeClass("past");
+    } else {
+      $(".time-block").addClass("future");
+      $(".time-block").removeClass("present");
+      $(".time-block").removeClass("past");
+    }
+  });
+}
