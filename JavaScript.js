a1f1fe871e702058957d3ce9f666d0f0585acb72
Clay <claymcintosh@gmail.com>
2022-01-13 10:55:42 +1000

    Local storage get and set seems to be working fine now.

--- JavaScript.js
+++ JavaScript.js
@@ -28,12 +28,85 @@ function timeColours() {
   });
 }
 
-//Save to local storage
+//Get local storage and save to local storage on click of save button
 
-let saveData = $("#aaa").val();
-
-console.log(saveData);
-
+//9am - get
+let g9 = localStorage.getItem("9am");
+$("#9am").val(g9);
+//9am - set
+let data9 = $("#9am");
 $(".saveBtn").on("click", function () {
-  localStorage.setItem("9am", saveData);
+  localStorage.setItem("9am", data9.val());
+});
+
+//10am - get
+let g10 = localStorage.getItem("10am");
+$("#10am").val(g10);
+//10am
+let data10 = $("#10am");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("10am", data10.val());
+});
+
+//11am - get
+let g11 = localStorage.getItem("11am");
+$("#11am").val(g11);
+//11am - set
+let data11 = $("#11am");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("11am", data11.val());
+});
+
+//12pm - get
+let g12 = localStorage.getItem("12pm");
+$("#12pm").val(g12);
+//12pm - set
+let data12 = $("#12pm");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("12pm", data12.val());
+});
+
+//1pm - get
+let g1 = localStorage.getItem("1pm");
+$("#1pm").val(g1);
+//1pm - set
+let data1 = $("#1pm");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("1pm", data1.val());
+});
+
+//2pm - get
+let g2 = localStorage.getItem("2pm");
+$("#2pm").val(g2);
+//2pm - set
+let data2 = $("#2pm");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("2pm", data2.val());
+});
+
+//3pm - get
+let g3 = localStorage.getItem("3pm");
+$("#3pm").val(g3);
+//3pm - set
+let data3 = $("#3pm");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("3pm", data3.val());
+});
+
+//4pm - get
+let g4 = localStorage.getItem("4pm");
+$("#4pm").val(g4);
+//4pm - set
+let data4 = $("#4pm");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("4pm", data4.val());
+});
+
+//5pm - get
+let g5 = localStorage.getItem("5pm");
+$("#5pm").val(g5);
+//5pm - set
+let data5 = $("#5pm");
+$(".saveBtn").on("click", function () {
+  localStorage.setItem("5pm", data5.val());
 });
